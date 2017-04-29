fd97479f80642288a3a50f329a336a5e483bea9c
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-04-29 13:21:08 +0300

    Add Chart.js JavaScript charting library to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,12 +23,15 @@ import { LatencyComponent } from './latency/latency.component';
 import { ErrorsComponent } from './errors/errors.component';
 import { CustomComponent } from './custom/custom.component';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        ChartsModule
     ],
     declarations: [
         AppComponent,
