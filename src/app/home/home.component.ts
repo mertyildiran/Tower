c2ce74efe4ab6dffb30ec3db99e9b06602d368ac
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-04-29 22:18:12 +0300

    Fix Traffic vs Average Latency chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,19 +7,17 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 })
 
 export class HomeComponent implements OnInit {
-    data;
-    chart1Data;
-    chart1Labels;
 
-    // lineChart
-    public lineChartData:Array<any> = [
-      {data: [], label: 'Series A'}
+    // lineChart1
+    public lineChart1Data:Array<any> = [
+        {data: [], label: 'Traffic(byte)'},
+        {data: [], label: 'Latency(ms)'}
     ];
-    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    public lineChartOptions:any = {
+    public lineChart1Labels:Array<any> = new Array(60);
+    public lineChart1Options:any = {
       responsive: true
     };
-    public lineChartColors:Array<any> = [
+    public lineChart1Colors:Array<any> = [
       { // grey
         backgroundColor: 'rgba(148,159,177,0.2)',
         borderColor: 'rgba(148,159,177,1)',
@@ -45,16 +43,16 @@ export class HomeComponent implements OnInit {
         pointHoverBorderColor: 'rgba(148,159,177,0.8)'
       }
     ];
-    public lineChartLegend:boolean = true;
-    public lineChartType:string = 'line';
+    public lineChart1Legend:boolean = true;
+    public lineChart1Type:string = 'line';
 
 
     // events
-    public chartClicked(e:any):void {
+    public chart1Clicked(e:any):void {
       console.log(e);
     }
 
-    public chartHovered(e:any):void {
+    public chart1Hovered(e:any):void {
       console.log(e);
     }
 
@@ -64,32 +62,46 @@ export class HomeComponent implements OnInit {
         .subscribe((res) => {
              //do your operations with the response here
              res.sort((a, b) => new Date("2017-05-01T" + b.Time).getTime() - new Date("2017-05-01T" + a.Time).getTime()).reverse();
-             this.data = res.slice(0, 7);
 
-             this.chart1Data = this.data.map(function(element) {
-                 //return { x: new Date("2017-05-01T" + element['Time']), y: parseInt(element['Message Size(byte)'])};
-                 return parseInt(element['Message Size(byte)']);
-             });
-             this.chart1Labels = this.data.map(function(element) {
-                 return element['Time'];
-             });
+             var data = res; // Create a copy of the response
+             var thresh = new Date("2017-05-01T" + data.slice(-1)[0]['Time']); // Get the last datetime as threshold
+             thresh.setTime(thresh.getTime() - (60*60*1000)); // Set the threshold one hour back
+             var data_filtered = []; // Create an empty filtered data for push
+             data.forEach(function(element) {
+                if (new Date("2017-05-01T" + element['Time']).getTime() > thresh.getTime()) {
+                    data_filtered.push(element);
+                }
+            });
 
-             this.lineChartData = [
-               {data: this.chart1Data, label: 'Series A'}
+            var this_min = new Date("2017-05-01T" + data_filtered[0]['Time']); // Get the datetime of first element
+            var traffic_per_min = [];
+            var latency_per_min = [];
+            var subtotal = 0;
+            var subtotal2 = 0;
+            var counter = 0;
+            data_filtered.forEach(function(element) {
+                if ( (new Date("2017-05-01T" + element['Time']).getTime() - this_min.getTime()) > (1*60*1000) ) { // If current element's datetime is more than a minute ahead of current this_min
+                    this_min.setTime(this_min.getTime() + (1*60*1000)); // Update this_min = this_min + 1 minute
+                    traffic_per_min.push(subtotal/counter); // Push sub-average
+                    latency_per_min.push(subtotal2/counter); // Push sub-average
+                    // Reset the variables
+                    subtotal = 0;
+                    subtotal2 = 0;
+                    counter = 0;
+                }
+                subtotal += parseInt(element['Message Size(byte)']); // Each turn increase subtotal by Message Size
+                subtotal2 += parseInt(element['Response Time(ms)']); // Each turn increase subtotal by Response Time
+                counter += 1;
+            });
+
+             this.lineChart1Data = [ // Update the chart data
+               {data: traffic_per_min, label: 'Traffic(byte)'},
+               {data: latency_per_min, label: 'Average Latency(ms)'}
              ];
-             console.dir(this.lineChartLabels);
-             this.lineChartLabels = this.chart1Labels;
 
-             this.printTheData();
             }
         );
     }
-
-      printTheData(){
-          //console.dir(this.data);
-          //console.dir(this.chart1Data);
-          console.dir(this.lineChartLabels);
-      }
 
       ngOnInit() {
       }
